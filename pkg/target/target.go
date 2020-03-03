61814bdb870376cc3a8e686d1b64b9a1bc0d8250
Igor Komlew <igor.komlew@gmail.com>
2020-03-03 20:49:14 +0100

    wip

--- pkg/target/target.go
+++ pkg/target/target.go
@@ -2,7 +2,9 @@ package target
 
 import (
 	"context"
+	"sync"
 
+	"github.com/rs/zerolog/log"
 	"github.com/sevigo/hokan/pkg/core"
 	"github.com/sevigo/hokan/pkg/target/void"
 )
@@ -12,12 +14,34 @@ var targets = map[string]core.TargetFactory{
 	void.TargetName: void.New,
 }
 
-func New(ctx context.Context, fileStore core.FileStore, event core.EventCreator) {
+var registerM sync.Mutex
+var register map[string]core.TargetStorage = make(map[string]core.TargetStorage)
 
+// type Target struct {
+// 	ctx      context.Context
+// 	event    core.EventCreator
+// 	store    core.DirectoryStore
+// 	notifier core.Notifier
+// 	catalog  []*core.Directory
+// }
+
+func New(ctx context.Context, fileStore *core.FileStore, event core.EventCreator) {
+	initTargets(fileStore)
 }
 
-func initTargets(fileStore core.FileStore) {
-	for _, target := range targets {
-
+func initTargets(fileStore *core.FileStore) {
+	for name, target := range targets {
+		ts, err := target(fileStore)
+		if err != nil {
+			log.Err(err).Msg("Can't create new target storage")
+			continue
+		}
+		addTarget(name, ts)
 	}
+}
+
+func addTarget(name string, ts core.TargetStorage) {
+	registerM.Lock()
+	defer registerM.Unlock()
+	register[name] = ts
 }
