a78e1ced4c90f0b9ad903ba235ffc0cae6692614
Igor Komlew <igor.komlew@gmail.com>
2020-03-05 22:11:40 +0100

    fixed error

--- pkg/store/file/file.go
+++ pkg/store/file/file.go
@@ -41,8 +41,8 @@ func (s *fileStore) Find(ctx context.Context, bucketName, filePath string) (*cor
 		return nil, errors.New("entry was not found")
 	}
 
-	errJson := json.NewDecoder(bytes.NewReader(value)).Decode(file)
-	return file, errJson
+	errJ := json.NewDecoder(bytes.NewReader(value)).Decode(file)
+	return file, errJ
 }
 
 func (s *fileStore) Save(ctx context.Context, bucketName string, file *core.File) error {
