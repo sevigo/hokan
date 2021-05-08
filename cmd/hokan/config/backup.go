2981ac806ba6904be3170d06c7c8fa981e984f9d
Igor Komlew <igor.komlew@gmail.com>
2021-05-08 23:34:56 +0200

    Added default config for MinIO server (#67)

--- cmd/hokan/config/backup.go
+++ cmd/hokan/config/backup.go
@@ -3,6 +3,9 @@ package config
 import (
 	"path"
 	"runtime"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
 func defaultViodBackup() Backup {
@@ -27,4 +30,25 @@ func defaultLocalBackup() Backup {
 
 func defaultBackup(c *Config) {
 	c.Backup = defaultViodBackup()
+}
+
+// TODO: we will read this config later from the UI maybe?
+func defaultMinIO(c *Config) {
+	viper.SetConfigType("yml")
+	if c.Backup.configName == "" {
+		c.Backup.configName = "config"
+	}
+	viper.SetConfigName(c.Backup.configName)
+	if c.Backup.configPath == "" {
+		c.Backup.configPath = "."
+	}
+	viper.AddConfigPath(c.Backup.configPath)
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.WithError(err).Fatal("Error reading config file")
+	}
+	err = viper.Unmarshal(&c.Backup)
+	if err != nil {
+		log.WithError(err).Fatal("Error unmarshaling config file")
+	}
 }
